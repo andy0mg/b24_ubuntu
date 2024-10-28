15ca1d9ef1e365162e29a684c297938464f49eb6
andy0mg <40331027+andy0mg@users.noreply.github.com>
2024-10-28 21:46:22 +0300

    Update zbx_graf_prom.sh

--- zbx_graf_prom.sh
+++ zbx_graf_prom.sh
@@ -139,7 +139,7 @@ prom_conf() {
 
 if echo $os|grep -Eo 'Ubuntu' >/dev/null
 then
-apt install apt-transport-https ca-certificates curl software-properties-common
+apt install -y apt-transport-https ca-certificates curl software-properties-common git
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt update

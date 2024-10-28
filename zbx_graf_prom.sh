987209a54b390b67a3db42345e86e77ccfbce64b
andy0mg <40331027+andy0mg@users.noreply.github.com>
2024-10-28 21:56:28 +0300

    Update zbx_graf_prom.sh

--- zbx_graf_prom.sh
+++ zbx_graf_prom.sh
@@ -142,7 +142,7 @@ then
 apt install -y apt-transport-https ca-certificates curl software-properties-common git
 mkdir -m 0755 -p /etc/apt/keyrings/
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [signed-by=/etc/apt/keyrings/docker.gpg]  [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
 apt -y install docker-ce docker-ce-cli containerd.io docker-compose
 git clone https://github.com/zabbix/zabbix-docker.git

1a833e1fc0daa82afafbb01b16c9b48cbca90907
andy0mg <40331027+andy0mg@users.noreply.github.com>
2025-08-09 12:16:30 +0300

    Update percona84.sh

--- percona84.sh
+++ percona84.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 
-# bash <(curl -sL https://raw.githubusercontent.com/andy0mg/b24_ubuntu/refs/heads/main/percona8.sh)
+# bash <(curl -sL https://raw.githubusercontent.com/andy0mg/b24_ubuntu/refs/heads/main/percona84.sh)
 
 cat > /root/run.sh <<\END
 
